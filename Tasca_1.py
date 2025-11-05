a43c30c757c020eefd45788813965e14d27aee6d
Laia <laia.cabrera09@iesantpol.cat>
2025-11-05 12:38:07 +0000

    amb lo del àdria i una mica la tasca

--- Tasca_1.py
+++ Tasca_1.py
@@ -1,4 +1,41 @@
+a_0 = float(7.5)
+a_1 = int(3)
+a_2 = int(4)
+a_3 = int(2)
 
+print(f"De l'equació: ({a_3}x³+{a_2}x²-{a_1}x)/{a_0} amb dos intents per intentar determinar una de les arrels de l'equació")
 
+x = int(input("Intenta primer amb un decímal: "))
+x_1 = x
+x_2 = x*x
+x_3 = x*x*x
 
-print("De l'equació ")
+opera = print(float((a_3*x_3+a_2*x_2-a_1*x_1)/{a_0}))
+
+if opera == 0:
+    boolea_1 = bool(True)
+    print(f"Molt bé has descobert una de les arrels")
+
+else:
+    boolea_1 = bool(False)
+    print(f"Làstima, tens un altre intent")
+
+x1 = int(input("Ara prova amb un enter: "))
+x1_1 = x1
+x1_2 = x1*x1
+x1_3 = x1*x1*x1
+
+opera = print(float((a_3*x1_3+a_2*x1_2-a_1*x1_1)/{a_0}))
+
+if opera == 0:
+    boolea_2 = bool(True)
+    print(f"Molt bé has descobert una de les arrels, en el segon intent")
+
+elif boolea_1 == True and opera == 0:
+    boolea_2 = bool(True)
+    print("Molt bé has trobat dues arrels")
+
+else:
+    boolea_2 = bool(False)
+    print(f"Làstima, t'ha quedat sense intents")
+
--- main.py
+++ main.py
@@ -3,3 +3,70 @@ print("Hola mòn")
 x = input("Numero del 1 al 10: ")
    
 print(f"{float(x)-2}, es el resultat del teu nombre -2")
+
+
+import random
+
+# Això és un comentari
+"""
+Això és un bloc de comentari
+
+Puc escriure a totes les línies.
+"""
+
+"""
+TIPUS DE VARIABLES:
+* Nombres: Enters (int) i Decimals (float)
+* Text: Cadena de caràcters (str)
+* Booleans: Veritat o fals (bool)
+
+Anem a declarar les variables:
+Variables: en minúscula
+CONSTANTS: en majúscula
+"""
+enter_1 = 17 # És una variable entera
+float_1 = 3.
+
+string_1 = "Això és un string"
+string_2 = 'Un altre string'
+
+boolea_1 = True
+boolea_2 = False
+
+print("Vull imprimir aquest missatge per pantalla")
+print(string_1)
+print(string_2)
+
+print(type(enter_1))
+print(type(float_1))
+print(type(string_1))
+print(type(boolea_1))
+print(type(print))
+print(random.random)
+
+print(f"Això és un enter: {enter_1}, això és un float: {float_1}")
+
+# Operadors aritmètics
+operacio = enter_1 - float_1 + 4
+print(f"L'operació té com a resultat: {operacio} i és de tipus: {type(operacio)}")
+
+operacio_complexa = enter_1**5 + float_1/5 - 3*(enter_1 % 4)
+print(operacio_complexa)
+
+print(1%5, 2%5, 3%5, 4%5, 5%5, 6%5)
+
+enter_2 = input("Introdueix un nombre enter: ")
+
+print(enter_2)
+
+
+
+
+# CTRL + ç (comento el bloc de codi)
+
+# # import random
+# import numpy as np
+# import random
+# y = random.choices()
+
+# # print(y)

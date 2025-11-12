d13360fe37a2503ed4564d1cf56c68fa586799dd
Laia <laia.cabrera09@iesantpol.cat>
2025-11-12 12:10:35 +0000

    FIN_T1

--- TASQUES2.py
+++ TASQUES2.py
@@ -5,39 +5,41 @@ estudiant = True
 
 print(f"Dades fixades: nom={nom}, edat={edat}, pes={pes}, estudiant={estudiant}")
 
-nom_per = input("Introdueix el teu nom: ")
+nom_per = input("\nIntrodueix el teu nom: ")
 edat_per = int(input("Introdueix la teva edat: "))
 pes_per = float(input("Introdueix el teu pes (kg): "))
 alt_per = float(input("Introdueix la teva altura (m): "))
 
-print("---- Informe de salut ----")
+print("\n---- Informe de salut ----")
 
 print(f"Nom: {nom_per}")
 print(f"Edat: {edat_per} (tipo: {type(edat_per)})")
-print(f"Pes: {pes_per} (tipo: {type(pes_per)})")
-print(f"Altura: {alt_per} (tipo: {type(alt_per)})")
+print(f"Pes: {pes_per} kg (tipo: {type(pes_per)})")
+print(f"Altura: {alt_per} m (tipo: {type(alt_per)})")
 
 imc = pes_per/(alt_per**2)
 
-print(f"Índex de massa corporal (IMC): {imc}")
+print(f"\nÍndex de massa corporal (IMC): {imc: .2f}")
 
 
 if imc <= 18.5:
-    IMC = "peso bajo"
+    IMC = "pes baix"
 
-elif 18.5 > imc <= 24.9:
-    IMC = "peso normal u óptimo"
+elif 18.5 < imc <= 24.9:
+    IMC = "pes normal o òptim"
 
-elif 25 >= imc <= 29.9:
-    IMC = "sobrepeso"
+elif 24.9 < imc <= 29.9:
+    IMC = "sobrepès"
 
-elif 30 >= imc <= 34.9:
-    IMC = "obesidad de clase I"
+elif 29.9 < imc <= 34.9:
+    IMC = "obesitat de classe I"
 
-elif 35.0 >= imc <= 39.9:
-    IMC = "obesidad de clase II"
+elif 34.9 < imc <= 39.9:
+    IMC = "obesitat de classe II"
 
-elif imc >= 40:
-    IMC = "obesidad de clase III o mórbida"
+elif imc > 39.9:
+    IMC = "obesitat de classe III o mòrbida"
 
 print(f"Classificació: {IMC}")
+
+print("----------------------------------")

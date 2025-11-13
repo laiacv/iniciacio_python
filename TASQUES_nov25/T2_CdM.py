63a0e2204c896e31ebed5bc44b4473866278df40
Laia <laia.cabrera09@iesantpol.cat>
2025-11-13 08:01:39 +0000

    fi T2

--- TASQUES_nov25/T2_CdM.py
+++ TASQUES_nov25/T2_CdM.py
@@ -4,4 +4,6 @@ print(f"Taxa de canvi fixa: {TCF}")
 
 diners = float(input("Introdueix l'import en euros: "))
 
+dollars = diners * 1.10
 
+print(f"\n{diners: .2f} € equivalen a {dollars: .2f} $ (tipo entrada: {type(diners)}, tipo resultat: {type(dollars)})")
